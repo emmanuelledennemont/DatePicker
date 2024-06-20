711afbd3f8e68e5dc8b8081106efe5f6c1c890d3
emmanuelle <emmanuelledennemont@orange.fr>
2024-06-20 14:41:45 +0400

    Initial Commit

---
+++ DatePicker/DatePickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  DatePickerApp.swift
+//  DatePicker
+//
+//  Created by Emmanuelle  Dennemont on 20/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct DatePickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
