c5b24d3570c6d187e9ec584c41c2f3e7476d9c1b
emmanuelle <emmanuelledennemont@orange.fr>
2024-06-20 15:19:35 +0400

    refacto  ajout des boutons

--- DatePicker/ContentView.swift
+++ DatePicker/ContentView.swift
@@ -2,6 +2,7 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var selectedDate = Date()
+    @State private var originalDate = Date() // Pour stocker la date d'origine en cas d'annulation
 
     var body: some View {
         VStack {
@@ -16,8 +17,19 @@ struct ContentView: View {
 
             Text("Date sélectionnée : \(selectedDate, formatter: dateFormatter)")
                 .font(.headline)
-                .foregroundStyle(.orange) // Changer la couleur du texte de la date sélectionnée
+                .foregroundColor(.orange) // Changer la couleur du texte de la date sélectionnée
                 .padding()
+
+            CustomButton(title: "Confirmer", action: {
+                // Confirmer : enregistrer la date sélectionnée
+                originalDate = selectedDate
+                // Vous pouvez ajouter ici le code pour enregistrer la date dans votre modèle de données ou effectuer d'autres actions nécessaires
+            }, isFilled: true)
+            
+            CustomButton(title: "Annuler", action: {
+                // Annuler : réinitialiser la date à sa valeur d'origine
+                selectedDate = originalDate
+            }, isFilled: false)
 
             Spacer()
         }
@@ -33,6 +45,29 @@ struct ContentView: View {
     }()
 }
 
+struct CustomButton: View {
+    let title: String
+    let action: () -> Void
+    let isFilled: Bool
+
+    var body: some View {
+        Button(action: action) {
+            Text(title)
+                .foregroundColor(isFilled ? .white : .orange)
+                .frame(maxWidth: .infinity)
+                .padding()
+                .background(isFilled ? Color.orange : Color.clear)
+                .cornerRadius(10)
+                .overlay(
+                    RoundedRectangle(cornerRadius: 10)
+                        .stroke(Color.orange, lineWidth: isFilled ? 0 : 2)
+                )
+        }
+        .padding()
+    }
+}
+
+
 #Preview {
-ContentView()
+    ContentView()
 }
